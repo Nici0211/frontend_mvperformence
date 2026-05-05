8b278ef3652e4ec04e4e98544276f87aea2019c6
harjab22 <harjab22@htl-kaindorf.at>
2026-05-05 10:14:04 +0200

    ServicesPage - Text hinzugefügt -> warten auf Offersapi

--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -1,4 +1,28 @@
+import {Button} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+
 export default function ServicesPage() {
   // Alle Leistungen der Werkstatt (Oelwechsel, Reifenwechsel, Bremsservice, Begutachtung, etc.)
-  return <></>;
+    const navigate = useNavigate();
+
+
+  return <>
+      <div>
+          <h1>KFZ-Technik GDG - <br></br> Ihre Werkstatt <br></br> in Leibnitz</h1>
+          <p>Die Autowerkstatt, die Leibnitz vertraut.</p>
+          <Button variant="contained" onClick={() => navigate("/termin")}>
+              Termin anfragen
+          </Button>
+      </div>
+
+      <div>
+          <p>Was wir anbieten</p>
+          <h2>Unsere Leistungen</h2>
+          <p>Professionelle KFZ-Arbeiten - schnell, transparent und zu fairen Preisen.</p>
+      </div>
+
+      <div>
+
+      </div>
+  </>;
 }
