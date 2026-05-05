3313e361df6e35f4fe43a1acc4226da4bd9236f2
domi597 <randob22@htl-kaindorf.at>
2026-05-05 10:17:20 +0200

    services api

--- src/api/services.ts
+++ src/api/services.ts
@@ -1,8 +1,35 @@
 import api from "./api";
 
-// TODO: Leistungen Endpoints
-// GET    /api/leistungen
-// GET    /api/leistungen/:id
-// POST   /api/leistungen
-// PUT    /api/leistungen/:id
-// DELETE /api/leistungen/:id
+/*
+* NAME : Dominik Ranegger
+* DATE : 05.05
+* */
+
+
+export interface IService {
+    id?: number;
+    icon: string;
+    title: string;
+    subtitle?: string;
+    sort: number;
+}
+
+export const fetchServices = (): Promise<IService[]> =>
+    api.get<IService[]>("/api/services").then((r) => r.data);
+
+export const fetchServiceById = (id: number): Promise<IService> =>
+    api.get<IService>(`/api/services/${id}`).then((r) => r.data);
+
+export const createService = (
+    body: Omit<IService, "id">
+): Promise<IService> =>
+    api.post<IService>("/api/services", body).then((r) => r.data);
+
+export const updateService = (
+    id: number,
+    body: Omit<IService, "id">
+): Promise<IService> =>
+    api.put<IService>(`/api/services/${id}`, body).then((r) => r.data);
+
+export const deleteService = (id: number): Promise<void> =>
+    api.delete(`/api/services/${id}`).then(() => undefined);
